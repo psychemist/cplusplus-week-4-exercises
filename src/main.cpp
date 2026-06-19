c152ddb13f032cd78f35894924b6d57b5e7c3f76
psychemist <ikeagudike@gmail.com>
2026-06-19 19:34:49 +0100

    feat: add all solutions

--- src/main.cpp
+++ src/main.cpp
@@ -13,52 +13,63 @@ bool UTXO::operator==(const UTXO& other) const {
 std::vector<std::string> parse_txid(const std::string& txid) {
     // TODO: split `txid` into 2-character chunks and return them as a vector
     //       of strings. Example: "aabbccdd" -> {"aa", "bb", "cc", "dd"}.
-    (void)txid;
-    return {};
+    std::vector<std::string> result;
+    for (size_t i = 0; i < txid.length(); i += 2) {
+        result.push_back(txid.substr(i, 2));
+    }
+    return result;
 }
 
 UTXO create_utxo(const std::string& txid, uint32_t vout, double amount) {
     // TODO: construct a UTXO from the three arguments.
-    (void)txid;
-    (void)vout;
-    (void)amount;
-    return {};
+    UTXO u;
+    u.txid = txid;
+    u.vout = vout;
+    u.amount = amount;
+    return u;
 }
 
 void update_utxo(UTXO& utxo, double new_amount) {
     // TODO: update `utxo.amount` in place to `new_amount`.
-    (void)utxo;
-    (void)new_amount;
+    utxo.amount = new_amount;
 }
 
 std::string unpack_utxo(const UTXO& utxo) {
     // TODO: return a string of the form
     //       "TXID: <txid>, VOUT: <vout>, AMOUNT: <amount>"
     //       built from `utxo`'s fields.
-    (void)utxo;
-    return "";
+    std::string vout_str = std::to_string(utxo.vout);
+    std::string amount_str = std::to_string(utxo.amount);
+    std::string result = "TXID: " + utxo.txid + ", VOUT: " + vout_str + ", AMOUNT: " + amount_str;
+    return result;
 }
 
 std::pair<std::string, std::string> swap_addresses(const std::string& a,
                                                    const std::string& b) {
     // TODO: return the two addresses swapped.
-    (void)a;
-    (void)b;
-    return {};
+    std::pair<std::string, std::string> p;
+    p.first = b;
+    p.second = a;
+    return p;
 }
 
 std::unordered_set<std::string> unique_addresses(
-    const std::vector<std::string>& addresses) {
     // TODO: return a set containing each distinct address from `addresses`.
-    (void)addresses;
-    return {};
+    const std::vector<std::string>& addresses) {
+    std::unordered_set<std::string> result;
+    for (size_t i = 0; i < addresses.size(); ++i) {
+        result.insert(addresses[i]);
+    }
+    return result;
 }
 
 std::vector<uint32_t> block_height_generator(uint32_t start, uint32_t end) {
     // TODO: return every height in the half-open range [start, end).
-    (void)start;
-    (void)end;
-    return {};
+    std::vector<uint32_t> result;
+    for (uint32_t i = start; i < end; ++i) {
+        result.push_back(i);
+    }
+    return result;
 }
 
 // ---- BitcoinWallet ----------------------------------------------------------
@@ -66,12 +77,17 @@ std::vector<uint32_t> block_height_generator(uint32_t start, uint32_t end) {
 void BitcoinWallet::add_utxo(const UTXO& utxo) {
     // TODO: store `utxo` in `utxos`, keyed by "<txid>:<vout>".
     //       Hint: std::to_string for the vout component.
-    (void)utxo;
+    std::string key = utxo.txid + ":" + std::to_string(utxo.vout);
+    utxos[key] = utxo;
 }
 
 double BitcoinWallet::get_balance() const {
     // TODO: sum the `amount` of every UTXO in `utxos`.
-    return 0.0;
+    double total = 0.0;
+    for (const auto& pair : utxos) {
+        total += pair.second.amount;
+    }
+    return total;
 }
 
 // ---- TransactionPool --------------------------------------------------------
@@ -80,11 +96,11 @@ bool TransactionPool::add_transaction(const std::string& txid) {
     // TODO: insert `txid` into `tx_pool`. Return true if it was newly
     //       inserted, false if it was already present.
     //       Hint: std::unordered_set::insert returns {iterator, bool}.
-    (void)txid;
-    return false;
+    auto result = tx_pool.insert(txid);
+    return result.second;
 }
 
 std::size_t TransactionPool::get_pool_size() const {
     // TODO: return the number of transactions in `tx_pool`.
-    return 0;
+    return tx_pool.size();
 }
